a2412c583b011d742a7cb4d7939e35fc0f5ea1ca
Henry Marinho <henrymarinho90@gmail.com>
2026-01-15 20:46:26 -0500

    fix add site url and optimize images

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,16 @@ import solidFs from '@astrojs/solid-js';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  // 👇 ESTA LÍNEA ES LA LLAVE MAESTRA. SIN ELLA, EL BUILD FALLA.
+  // 👇 ESTA LÍNEA ES LA LLAVE. SIN ELLA, NO HAY WEB.
   site: 'https://portfolio.romahomestore.com',
 
+  // Adaptador Node en modo Standalone (Correcto para Coolify)
   adapter: node({
     mode: 'standalone',
   }),
 
   integrations: [
-    Unocss({
-      injectReset: true,
-    }),
+    Unocss({ injectReset: true }),
     svelte(),
     solidFs(),
     sitemap(),
